a7ae66763c942a90d48346d7ebbedd3db51dcd8b
oussamaelfouah <oelfouah@gmail.com>
2023-11-25 16:28:15 +0100

    dom

---
+++ favoret pet/hello.js
@@ -0,0 +1,7 @@
+let image = document.getElementById("img");
+    let message = document.getElementById("message");
+    image.addEventListener('click', text);
+    function text (){
+        message.textContent = "you have clicked to the image"
+
+    }
